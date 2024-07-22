c846504a5e9573518ec5914bdcef67f084429b3e
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-07-22 16:10:30 -0400

    feat: add console panic hook

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use koan::{error::handle_err, interpreter::IntrpCtx, lexer::lex, parser::parse, state};
 use wasm_bindgen::prelude::*;
+use std::panic;
 
 #[wasm_bindgen]
 pub struct Output {
@@ -33,6 +34,8 @@ impl WState {
 
 #[wasm_bindgen]
 pub fn run_line(line: String, state: &mut WState) -> Result<Output, String> {
+    console_error_panic_hook::set_once();
+
     let (mut statements, pool) = lex(&line).and_then(parse).map_err(handle_err)?;
     let mut stdout: Vec<u8> = vec![];
     let mut ctx = IntrpCtx {
